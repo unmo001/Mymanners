80fbc6f3ab0505a4d51c3e9022cafa85b87803b9
unmo001 <unmo001@gmail.com>
2022-06-09 17:29:15 +0900

    StartActivityの 始めるボタンを押した後tutorialFragmentを起動したいのですがうまくいきません。ご確認お願いします。

--- app/src/main/java/com/example/mymannars/ui/start/StartActivity.kt
+++ app/src/main/java/com/example/mymannars/ui/start/StartActivity.kt
@@ -1,10 +1,11 @@
 package com.example.mymannars.ui.start
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mymannars.R
 import com.example.mymannars.databinding.ActivityStartBinding
-
+import com.example.mymannars.ui.tutorial.TutoralFragment
 class StartActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +13,13 @@ class StartActivity : AppCompatActivity() {
         binding = ActivityStartBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+       binding.startBtn.setOnClickListener{
+           supportFragmentManager.beginTransaction().apply{
+               replace(com.google.android.material.R.id.container,TutoralFragment())
+               addToBackStack(null)
+               commit()
+           }
+       }
     }
 }
